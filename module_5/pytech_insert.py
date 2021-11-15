ac01b52dc03190f02ed2fd50dd508e74f07370a6
Vincent Sarlo <vincent.j.sarlo@gmail.com>
2021-11-14 22:47:11 -0500

    Assignment 5.3 of Module 5

---
+++ module_5/pytech_insert.py
@@ -0,0 +1,48 @@
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@sarlo-assignment4-2.5vuly.mongodb.net/pytech"
+
+client = MongoClient(url, ssl=True,ssl_cert_reqs='CERT_NONE')
+
+db = client.pytech
+
+student_collection = db.students
+
+print(db.list_collection_names())
+
+print("\n- - INSERT STATEMENTS - -")
+
+def insert_student(student):
+    student_doc_id = student_collection.insert_one(student).inserted_id
+    student_name = f'{student["first_name"]} {student["last_name"]}'
+    print(f"Inserted student record {student_name} into the student's collection with document_id {student_doc_id}")
+
+
+student_1007 = {
+    "student_id": "1007",
+    "first_name": "Thor",
+    "last_name": "Odinson",
+    "enrollment": [],
+    "courses": []
+}
+insert_student(student_1007)
+
+student_1008 = {
+    "student_id": "1008",
+    "first_name": "Steve",
+    "last_name": "Rodgers",
+    "enrollment": [],
+    "courses": []
+}
+insert_student(student_1008)
+
+student_1009 = {
+    "student_id": "1009",
+    "first_name": "Tony",
+    "last_name": "Stark",
+    "enrollment": [],
+    "courses": []
+}
+insert_student(student_1009)
+
+print("\n\nEnd of program, press any key to exit . . .\n")
---
+++ module_5/pytech_queries.py
@@ -0,0 +1,32 @@
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@sarlo-assignment4-2.5vuly.mongodb.net/pytech"
+
+client = MongoClient(url, ssl=True,ssl_cert_reqs='CERT_NONE')
+
+db = client.pytech
+
+col_pytech = db.students
+
+print(db.list_collection_names())
+
+
+docs = db.students.find({})
+
+print(f'-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --')
+
+for doc in docs:
+   # print(f"Student ID: {doc("student_name")}")
+   print(f'Student ID: {doc["student_id"]}')
+   print(f'First Name: {doc["first_name"]}')
+   print(f'Last Name: {doc["last_name"]}\n')
+
+
+doc = db.students.find_one({"student_id": "1008"})
+
+print(f'-- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --')
+print(f'Student ID: {doc["student_id"]}')
+print(f'First Name: {doc["first_name"]}')
+print(f'Last Name: {doc["last_name"]}\n')
+
+print(f'\nEnd of program, press any key to continue . . . \n')
