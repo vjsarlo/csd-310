dabec33ecb2b182e568e4d19301b45d9b24d033a
vjsarlo <vjsarlo@gmail.com>
2022-04-27 10:59:15 -0400

    Update willsonFinancial10.3.2.py

--- module_10/charlie group/assignment 10.3/willsonFinancial10.3PYTHON/willsonFinancial10.3.2.py
+++ module_10/charlie group/assignment 10.3/willsonFinancial10.3PYTHON/willsonFinancial10.3.2.py
@@ -3,7 +3,7 @@ from mysql.connector import errorcode
 
 config = {
     "user": "root",
-    "password": "Qexeoymp4123!",
+    "password": "x",
     "host": "127.0.0.1",
     "database": "willsonFinancial",
     "raise_on_warnings": True
